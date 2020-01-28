67c07cd1defaca6c2c4f294ead3c69f1bb5301e1
Austin Davis <austin.davis@sigaostudios.com>
2020-01-28 11:39:57 -0600

    Added functions for classifying the BinaryLike.t type.

--- src/BinaryLike.bs.js
+++ src/BinaryLike.bs.js
@@ -5,80 +5,69 @@ var Block = require("bs-platform/lib/js/block.js");
 var Pervasives = require("bs-platform/lib/js/pervasives.js");
 
 
-function reflectJsConstructor(data) {
+function jsReflectClass(data) {
   let str = Object.prototype.toString.call(data);
-  return str.slice(8, str.length - 1);
-};
+  return str.slice(8, -1);
+}
 
 ;
 
 var Unsafe = { };
 
-function classify_exn(binaryLike) {
-  var match = reflectJsConstructor(binaryLike);
-  switch (match) {
-    case "ArrayBuffer" :
-        return /* Buffer */Block.__(1, [binaryLike]);
-    case "DataView" :
-        return /* DataView */Block.__(11, [binaryLike]);
-    case "Float32Array" :
-        return /* Float32Array */Block.__(9, [binaryLike]);
-    case "Float64Array" :
-        return /* Float64Array */Block.__(10, [binaryLike]);
-    case "Int16Array" :
-        return /* Int16Array */Block.__(6, [binaryLike]);
-    case "Int32Array" :
-        return /* Int32Array */Block.__(8, [binaryLike]);
-    case "Int8Array" :
-        return /* Int8Array */Block.__(2, [binaryLike]);
-    case "String" :
-        return /* String */Block.__(0, [binaryLike]);
-    case "Uint16Array" :
-        return /* Uint16Array */Block.__(5, [binaryLike]);
-    case "Uint32Array" :
-        return /* Uint32Array */Block.__(7, [binaryLike]);
-    case "Uint8Array" :
-        return /* Uint8Array */Block.__(3, [binaryLike]);
-    case "Uint8ClampedArray" :
-        return /* Uint8ClampedArray */Block.__(4, [binaryLike]);
-    default:
-      return Pervasives.failwith("Unknown Data Type");
+function classifyOpt(binaryLike) {
+  if (typeof binaryLike === "string") {
+    return /* String */Block.__(0, [binaryLike]);
+  } else if (Buffer.isBuffer(binaryLike)) {
+    return /* Buffer */Block.__(1, [binaryLike]);
+  } else {
+    var match = jsReflectClass(binaryLike);
+    switch (match) {
+      case "DataView" :
+          return /* DataView */Block.__(11, [binaryLike]);
+      case "Float32Array" :
+          return /* Float32Array */Block.__(9, [binaryLike]);
+      case "Float64Array" :
+          return /* Float64Array */Block.__(10, [binaryLike]);
+      case "Int16Array" :
+          return /* Int16Array */Block.__(6, [binaryLike]);
+      case "Int32Array" :
+          return /* Int32Array */Block.__(8, [binaryLike]);
+      case "Int8Array" :
+          return /* Int8Array */Block.__(2, [binaryLike]);
+      case "Uint16Array" :
+          return /* Uint16Array */Block.__(5, [binaryLike]);
+      case "Uint32Array" :
+          return /* Uint32Array */Block.__(7, [binaryLike]);
+      case "Uint8Array" :
+          return /* Uint8Array */Block.__(3, [binaryLike]);
+      case "Uint8ClampedArray" :
+          return /* Uint8ClampedArray */Block.__(4, [binaryLike]);
+      default:
+        return ;
+    }
+  }
+}
+
+function classifyExn(binaryLike) {
+  var match = classifyOpt(binaryLike);
+  if (match !== undefined) {
+    return match;
+  } else {
+    return Pervasives.failwith("Unknown Data Type");
   }
 }
 
 function classify(binaryLike) {
-  var match = reflectJsConstructor(binaryLike);
-  switch (match) {
-    case "ArrayBuffer" :
-        return /* Ok */Block.__(0, [/* Buffer */Block.__(1, [binaryLike])]);
-    case "DataView" :
-        return /* Ok */Block.__(0, [/* DataView */Block.__(11, [binaryLike])]);
-    case "Float32Array" :
-        return /* Ok */Block.__(0, [/* Float32Array */Block.__(9, [binaryLike])]);
-    case "Float64Array" :
-        return /* Ok */Block.__(0, [/* Float64Array */Block.__(10, [binaryLike])]);
-    case "Int16Array" :
-        return /* Ok */Block.__(0, [/* Int16Array */Block.__(6, [binaryLike])]);
-    case "Int32Array" :
-        return /* Ok */Block.__(0, [/* Int32Array */Block.__(8, [binaryLike])]);
-    case "Int8Array" :
-        return /* Ok */Block.__(0, [/* Int8Array */Block.__(2, [binaryLike])]);
-    case "String" :
-        return /* Ok */Block.__(0, [/* String */Block.__(0, [binaryLike])]);
-    case "Uint16Array" :
-        return /* Ok */Block.__(0, [/* Uint16Array */Block.__(5, [binaryLike])]);
-    case "Uint32Array" :
-        return /* Ok */Block.__(0, [/* Uint32Array */Block.__(7, [binaryLike])]);
-    case "Uint8Array" :
-        return /* Ok */Block.__(0, [/* Uint8Array */Block.__(3, [binaryLike])]);
-    case "Uint8ClampedArray" :
-        return /* Ok */Block.__(0, [/* Uint8ClampedArray */Block.__(4, [binaryLike])]);
-    default:
-      return /* Error */Block.__(1, [binaryLike]);
+  var match = classifyOpt(binaryLike);
+  if (match !== undefined) {
+    return /* Ok */Block.__(0, [match]);
+  } else {
+    return /* Error */Block.__(1, [binaryLike]);
   }
 }
 
 exports.Unsafe = Unsafe;
-exports.classify_exn = classify_exn;
+exports.classifyOpt = classifyOpt;
+exports.classifyExn = classifyExn;
 exports.classify = classify;
 /*  Not a pure module */
--- src/BinaryLike.re
+++ src/BinaryLike.re
@@ -1,3 +1,8 @@
+/**
+ * TODO: Add lots of tests for these functions, expecially the
+ * `classify` funciton and its variants.
+ */
+
 open Js.TypedArray2;
 
 type t;
@@ -30,49 +35,71 @@ external float64Array: Float64Array.t => t = "%identity";
 external dataView: DataView.t => t = "%identity";
 
 [%%bs.raw {|
-function reflectJsConstructor(data) {
+function jsReflectClass(data) {
   let str = Object.prototype.toString.call(data);
-  return str.slice(8, str.length - 1);
-};
-|}];
+  return str.slice(8, -1);
+}
+|}]
+
+/**
+ * Note:
+ * 
+ * Accurate type reflection is a hard problem. This method is relatively
+ * standard, but relies on assumptions about how the class instance
+ * was created. In particular, it assumes that the standard constructor
+ * function was used (or official static constructor methods), and not
+ * an extension of the class inheriting from the base class. Extensions
+ * of a JS class may not be properly identified by the extension
+ * constructor's name.
+ * 
+ * There are all kinds of other quirks that can prevent accurate type
+ * reflection, such as manipulation of prototypes or constructors at
+ * runtime. Fortunately those issues are unlikely with these data types.
+ * 
+ * This works for most binary-like data types, but the class `Buffer` will
+ * normally appear as a `Uint8Array`, since that is the underlying data
+ * type. So we gueard that with the Node.js function, `Buffer.isBuffer`,
+ * to make sure we capture the `Buffer` type.
+ */
 
 module Unsafe = {
-  [@bs.val] external reflectJsConstructor: t => string = "reflectJsConstructor";
+  [@bs.val] external jsReflectClass: t => string = "jsReflectClass";
+  [@bs.get] [@bs.scope "constructor"] external jsConstructorName: t => string = "name";
 };
 
-let classify_exn: t => case = (binaryLike) => {
-  switch (Unsafe.reflectJsConstructor(binaryLike)) {
-    | "String" => String(Obj.magic(binaryLike));
-    | "ArrayBuffer" => Buffer(Obj.magic(binaryLike));
-    | "Int8Array" => Int8Array(Obj.magic(binaryLike));
-    | "Uint8Array" => Uint8Array(Obj.magic(binaryLike));
-    | "Uint8ClampedArray" => Uint8ClampedArray(Obj.magic(binaryLike));
-    | "Int16Array" => Int16Array(Obj.magic(binaryLike));
-    | "Uint16Array" => Uint16Array(Obj.magic(binaryLike));
-    | "Int32Array" => Int32Array(Obj.magic(binaryLike));
-    | "Uint32Array" => Uint32Array(Obj.magic(binaryLike));
-    | "Float32Array" => Float32Array(Obj.magic(binaryLike));
-    | "Float64Array" => Float64Array(Obj.magic(binaryLike));
-    | "DataView" => DataView(Obj.magic(binaryLike));
-    | _ => failwith("Unknown Data Type");
+let classifyOpt: t => option(case) = (binaryLike) => {
+  if (Js.typeof(binaryLike) === "string") {
+    Some(String(Obj.magic(binaryLike)))
+  } else if (Buffer.isBuffer(binaryLike)) {
+    Some(Buffer(Obj.magic(binaryLike)))
+  } else {
+    switch (Unsafe.jsReflectClass(binaryLike)) {
+      | "Int8Array" => Some(Int8Array(Obj.magic(binaryLike)));
+      | "Uint8Array" => Some(Uint8Array(Obj.magic(binaryLike)));
+      | "Uint8ClampedArray" => Some(Uint8ClampedArray(Obj.magic(binaryLike)));
+      | "Int16Array" => Some(Int16Array(Obj.magic(binaryLike)));
+      | "Uint16Array" => Some(Uint16Array(Obj.magic(binaryLike)));
+      | "Int32Array" => Some(Int32Array(Obj.magic(binaryLike)));
+      | "Uint32Array" => Some(Uint32Array(Obj.magic(binaryLike)));
+      | "Float32Array" => Some(Float32Array(Obj.magic(binaryLike)));
+      | "Float64Array" => Some(Float64Array(Obj.magic(binaryLike)));
+      | "DataView" => Some(DataView(Obj.magic(binaryLike)));
+      | _ => None;
+    };
+  };
+};
+
+let classifyExn: t => case = (binaryLike) => {
+  switch (classifyOpt(binaryLike)) {
+    | None => failwith("Unknown Data Type");
+    | Some(data) => data;
   };
 };
 
 let classify: t => Belt.Result.t(case, t) = (binaryLike) => {
-  switch (Unsafe.reflectJsConstructor(binaryLike)) {
-    | "String" => Ok(String(Obj.magic(binaryLike)));
-    | "ArrayBuffer" => Ok(Buffer(Obj.magic(binaryLike)));
-    | "Int8Array" => Ok(Int8Array(Obj.magic(binaryLike)));
-    | "Uint8Array" => Ok(Uint8Array(Obj.magic(binaryLike)));
-    | "Uint8ClampedArray" => Ok(Uint8ClampedArray(Obj.magic(binaryLike)));
-    | "Int16Array" => Ok(Int16Array(Obj.magic(binaryLike)));
-    | "Uint16Array" => Ok(Uint16Array(Obj.magic(binaryLike)));
-    | "Int32Array" => Ok(Int32Array(Obj.magic(binaryLike)));
-    | "Uint32Array" => Ok(Uint32Array(Obj.magic(binaryLike)));
-    | "Float32Array" => Ok(Float32Array(Obj.magic(binaryLike)));
-    | "Float64Array" => Ok(Float64Array(Obj.magic(binaryLike)));
-    | "DataView" => Ok(DataView(Obj.magic(binaryLike)));
-    | _ => Error(binaryLike);
+  switch (classifyOpt(binaryLike)) {
+    | None => Error(binaryLike);
+    | Some(data) => Ok(data);
   };
 };
 
